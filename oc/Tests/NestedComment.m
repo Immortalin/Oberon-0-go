613710c592fe0d29a92ff84deebcf3421d295e83
Tobias Schaer <toby.schaer@gmail.com>
2016-08-21 12:07:09 +0200

    Test for nested comments

---
+++ oc/Tests/NestedComment.m
@@ -0,0 +1,11 @@
+(* 
+Nested comment Test Module
+Inside this comment (* There is another comment *) 
+*)
+
+MODULE NestedComment;
+
+BEGIN
+    (* Write to output to show program has run successfully *)
+    Write(1)
+END NestedComment.
