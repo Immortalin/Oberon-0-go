d7e5495b7edc2a37a811a59239487456f45b2775
Tobias Schaer <toby.schaer@gmail.com>
2016-08-23 21:02:30 +0200

    Tests for WriteLn() and WriteHex()

---
+++ oc/Tests/WriteHex.m
@@ -0,0 +1,7 @@
+MODULE WriteHex;
+
+BEGIN
+    WriteHex(10);
+    WriteHex(32);
+    WriteHex(511)
+END WriteHex.
---
+++ oc/Tests/WriteLn.m
@@ -0,0 +1,7 @@
+MODULE WriteLn;
+
+BEGIN
+    Write(1);
+    WriteLn;
+    Write(2)
+END WriteLn.
