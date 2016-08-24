5e28df881395665c56cbfc9c6877180b9b8a9c4e
Tobias Schaer <toby.schaer@gmail.com>
2016-08-24 12:41:14 +0200

    Updated Array test module for Issue #4
    Uses variables as well as constants as indices.

--- oc/Tests/Array.m
+++ oc/Tests/Array.m
@@ -1,9 +1,32 @@
 (* Array Test Module *)
+(* Expanded to handle const and var indices *)
 MODULE Array;
-    VAR a: ARRAY 2 OF INTEGER;
+    VAR 
+        a: ARRAY 5 OF INTEGER;
+        i: INTEGER;
 BEGIN
+    (* Constant indices *)
     a[0] := 7;
     a[1] := 3;
     Write(a[0]);
-    Write(a[1])
+    Write(a[1]);
+    
+    (* Variable indices *)
+    (* This doesnt crash but prints 4294967295 *)
+    i := 1;
+    a[i] := i;    
+    Write(a[i]);
+    
+    (* This doesnt crash but prints 4294967295 *)
+    i := 2;
+    a[i] := i;    
+    Write(a[i]);
+    
+    (* This crashes the compiler *)
+    i := 0;
+    a[i] := i;
+    Write(a[i]);
+    
+    WriteLn
+    
 END Array.
